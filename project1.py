087dd74fcec167a3aa534d19ec102a8d17cb7bcc
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2024-04-05 18:53:53 -0500

    Moved Project1-test code to Project 1

--- project1.py
+++ project1.py
@@ -1,107 +1,103 @@
 def frameChoice():
     costs = []
-    frame = input("Which Case Size would you like? Small, Medium, or Large? ").lower()
-    while True: 
-        if frame == "small": 
+    while True:
+        frame = input("Which Case Size would you like? Small, Medium, or Large? ").lower()
+        if frame == "small":
             cost = 120
             breakdown = "Case size: Small ($120)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif frame == "medium": 
+        elif frame == "medium":
             cost = 210
             breakdown = "Case Size: Medium ($210)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif frame == "large": 
+        elif frame == "large":
             cost = 350
             breakdown = "Case Size: Large ($350)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        else: 
+        else:
             print("That is not a valid case size, try again!")
-            frame = input("Which Case Size would like? Small, Medium, or Large? ").lower()
     return costs
 
 def hardDriveChoice():
     costs = []
-    hardDrive = input("Which Hard Drive would you like? Budget, Standard, or High End? ").lower()
-    while True: 
+    while True:
+        hardDrive = input("Which Hard Drive would you like? Budget, Standard, or High End? ").lower()
         if hardDrive == "budget":
             cost = 100
             breakdown = "Hard Drive: Budget - 1TB HDD ($100)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif hardDrive == "standard": 
+        elif hardDrive == "standard":
             cost = 200
             breakdown = "Hard Drive: Standard - 1TB SSD ($200)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif hardDrive == "high end": 
+        elif hardDrive == "high end":
             cost = 350
             breakdown = "Hard Drive: High End - 5TB SSD ($350)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        else: 
+        else:
             print("That is not a valid Hard Drive option, try again!")
-            hardDrive = input("Which Hard Drive would you like? Budget, Stardard, or High End? ").lower()
     return costs
 
 def graphicsCardChoice():
     costs = []
-    gCard = input("Which Graphics Card would you like? Budget, Standard, or High End? ").lower()
-    while True: 
+    while True:
+        gCard = input("Which Graphics Card would you like? Budget, Standard, or High End? ").lower()
         if gCard == "budget":
             cost = 300
             breakdown = "Graphics Card: Budget - RTX 3060 ($300)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif gCard == "standard": 
+        elif gCard == "standard":
             cost = 690
             breakdown = "Graphics Card: Standard - RTX 4070ti  ($690)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif gCard == "high end": 
+        elif gCard == "high end":
             cost = 1820
             breakdown = "Graphics Card: High End - RTX 4090 ($1820)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        else: 
+        else:
             print("That is not a valid Graphics Card option, try again!")
-            gCard = input("Which Graphics Card would you like? Budget, Standard, or High End? ").lower()
     return costs
 
 def ramChoice():
     costs = []
-    ram = input("How much RAM would you like? 8GB, 16GB, 32GB, 64GB, or 128GB? ").lower()
-    while True: 
+    while True:
+        ram = input("How much RAM would you like? 8GB, 16GB, 32GB, 64GB, or 128GB? ").lower()
         if ram == "8gb":
             cost = 44
             breakdown = "RAM - 8GB ($44)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif ram == "16gb": 
+        elif ram == "16gb":
             cost = 60
             breakdown = "RAM - 16GB  ($60)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif ram == "32gb": 
+        elif ram == "32gb":
             cost = 100
             breakdown = "RAM - 32GB ($100)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif ram == "64gb": 
+        elif ram == "64gb":
             cost = 125
             breakdown = "RAM - 64GB ($125)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        elif ram == "128gb": 
+        elif ram == "128gb":
             cost = 400
             breakdown = "RAM - 128GB ($400)\n"
-            costs.append((cost, breakdown))
+            costs = [(cost, breakdown)]
             break
-        else: 
+        else:
             print("That is not a valid RAM option, try again!")
-            ram = input("How much RAM would you like? 8GB, 16GB, 32GB, 64GB, or 128GB? (No spaces please!) ").lower()
     return costs
 
 def calculateTotalCost(costs):
@@ -112,10 +108,10 @@ def calculateTotalCost(costs):
 
 def buildDesktop():
     costs = []
-    costs.extend(frameChoice())
-    costs.extend(hardDriveChoice())
-    costs.extend(graphicsCardChoice())
-    costs.extend(ramChoice())
+    costs += frameChoice()
+    costs += hardDriveChoice()
+    costs += graphicsCardChoice()
+    costs += ramChoice()
 
     totalCost, breakdown = calculateTotalCost(costs)
 
@@ -125,16 +121,16 @@ def buildDesktop():
 def main():
     print("Welcome to Wayne's Computer Builder. Let's get started!")
 
-    while True: 
+    while True:
         computerType = input("What type of computer would you like to build, a laptop or desktop? ").lower()
 
-        if computerType == "desktop": 
+        if computerType == "desktop":
             buildDesktop()
             break
-        ## elif computerType == "laptop": 
-            ## buildLaptop()
-            break
-        else: 
+        ## elif computerType == "laptop":
+        ##     buildLaptop()
+        ##     break
+        else:
             print("I'm sorry, that is not a valid computer type, try again!")
 
 main()
