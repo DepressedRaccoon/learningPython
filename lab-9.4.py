b96936889c06b7e9147c0a195fa8dc84411eb0e3
SChafinIII <118332995+DepressedRaccoon@users.noreply.github.com>
2024-04-21 19:19:28 -0500

    Finished lab

--- lab-9.4.py
+++ lab-9.4.py
@@ -1,7 +1,14 @@
 def main():
     print("Begin program")
     # the rest of your code goes below
+    productPrices = [10.99, 13.74, 3.29, 5.69, 100.0, 89.99, 19.35, 14.25]
 
+    total = sum(productPrices)
 
+    average = total/len(productPrices)
+
+    averageRounded = round(average, 2)
+
+    print("Average product price:", averageRounded)
 
 main()
