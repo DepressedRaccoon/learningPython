62a377c355437e22ea69fc94418f78b3f660f4a2
SChafinIII <118332995+DepressedRaccoon@users.noreply.github.com>
2024-05-04 19:45:57 -0500

    Input Spell Info
    Changes the program to display information for spells based on their school, level, and damage type.

--- project2.py
+++ project2.py
@@ -11,8 +11,8 @@ def FindItem(lst, item):
 ## Display information for single item
 def DisplayInformation(keyList, dataList, index):
     if index != -1: 
-        print(f"Item: {keyList[index]}")
-        print("Data:")
+        print(f"Spell: {keyList[index]}")
+        print("Spell Data:")
         for i in range(len(dataList[index])):
             print(f"{dataList[index][i]}")
     else:
@@ -20,8 +20,8 @@ def DisplayInformation(keyList, dataList, index):
 
 ## Display all information 
 def DisplayAllInformation(keyList, dataList): 
-    print("All info: ")
-    print("Item\t\tData")
+    print("All spells: ")
+    print("Spell\t\tData")
     for i in range(len(keyList)):
         print(f"{keyList[i]}\t\t{dataList[i]}")
 
@@ -29,12 +29,12 @@ def DisplayAllInformation(keyList, dataList):
 def CompareTwoItems(keyList, dataList, index1, index2): 
     if index1 != -1 and index2 != -1:
         print(f"Comparing {keyList[index1]} and {keyList[index2]}:")
-        print("1. Data for item 1")
+        print("1. Spell Data for Spell 1")
         print(dataList[index1])
-        print("2. Data for item 2")
+        print("2. Spell Data for Spell 2")
         print(dataList[index2])
     else:
-        print("Invalid index value for one or both items")
+        print("Invalid index value for one or both spells")
 
 ## Ensure valid index 
 def ValidateIndex(lst, index): 
@@ -46,32 +46,32 @@ def ValidateIndex(lst, index):
 ## Create menu function to display menu
 def Menu(): 
     print("Menu:")
-    print("1. Display information for a single item")
-    print("2. Display all information for each item")
-    print("3. Compare two items")
+    print("1. Display information for a single spell")
+    print("2. Display all information for each spell")
+    print("3. Compare two spells")
     print("4. Exit")
 
 ## Create a main function / Basic template
 def Main(): 
 
     ## List creation
-    keyList = ["Something", "Something1", "Something2", "Something3", "Something4", "Something5"]
-    dataList = [[1,2,3], [3,5,6], [7,8,9], [10,11,12], [13,14,15], [16,17,18]]
+    keyList = ["fireball", "bindingice", "fly", "antagonize", "powerwordkill", "soulcage"]
+    dataList = [["School: Evocation","Level: 3rd","Damage Type: Fire"], ["School: Evocation","Level: 2nd","Damage Type: Cold"], ["School: Transmutation","Level: 3rd","Damage Type: None"], ["School: Enchantment","Level: 3rd","Damage Type: Psychic"], ["School: Enchantment", "Level: 9th", "Damage Type: Yes"], ["School: Necromancy", "Level: 6th", "Damage Type: None"]]
 
     while True: 
         ## Call Rubric menu function
         Menu()
-        choice = input("Enter your choice: ")
+        choice = input("Enter your Spell choice: ").lower().replace(" ", "")
 
         if choice == '1': 
-            item = input("Enter the item to display: ")
+            item = input("Enter the spell to display: ").lower().replace(" ", "")
             index = FindItem(keyList, item)
             DisplayInformation(keyList, dataList, index)
         elif choice == '2': 
             DisplayAllInformation(keyList, dataList)
         elif choice == '3':
-            item1 = input("Enter the first item: ")
-            item2 = input("Enter the second item: ")
+            item1 = input("Enter the first spell: ").lower().replace(" ", "")
+            item2 = input("Enter the second spell: ").lower().replace(" ", "")
             index1 = FindItem(keyList, item1)
             index2 = FindItem(keyList, item2)
             CompareTwoItems(keyList, dataList, index1, index2)
